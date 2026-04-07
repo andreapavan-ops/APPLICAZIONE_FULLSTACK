fc60a8b774c480646473ee54bbaec6e13c17e2b4
andreapavan-ops <andrea.pavan@allievi.itsdigitalacademy.com>
2026-04-07 21:49:35 +0200

    Struttura iniziale fullstack: API (backend Express) + WEB (frontend HTML/CSS/JS)

---
+++ WEB/script.js
@@ -0,0 +1,6 @@
+const API_URL = "http://localhost:3000";
+
+// Esempio: fetch tutti i post
+// fetch(`${API_URL}/api/post`)
+//     .then(res => res.json())
+//     .then(data => console.log(data));
